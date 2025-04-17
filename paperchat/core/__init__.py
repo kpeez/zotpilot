1baeabc3aa2d25fd8c5faa433d00b0b40eede013
kpuhger <krpuhger@gmail.com>
2025-04-17 10:33:46 -0700

    update __all__ exports to include process_pdf_document

--- paperchat/core/__init__.py
+++ paperchat/core/__init__.py
@@ -1,4 +1,5 @@
+from .ingestion import process_pdf_document
 from .rag_pipeline import RAGPipeline
 from .vector_store import VectorStore
 
-__all__ = ["RAGPipeline", "VectorStore"]
+__all__ = ["RAGPipeline", "VectorStore", "process_pdf_document"]
