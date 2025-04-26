9ad041380fa0c1bb7b5f8f1db6cc609b9522f056
kpuhger <krpuhger@gmail.com>
2025-04-25 23:09:04 -0700

    add get_embedding_model to exports

--- paperchat/core/__init__.py
+++ paperchat/core/__init__.py
@@ -1,5 +1,6 @@
+from .embeddings import get_embedding_model
 from .ingestion import process_pdf_document
 from .rag_pipeline import RAGPipeline
 from .vector_store import VectorStore
 
-__all__ = ["RAGPipeline", "VectorStore", "process_pdf_document"]
+__all__ = ["RAGPipeline", "VectorStore", "get_embedding_model", "process_pdf_document"]
